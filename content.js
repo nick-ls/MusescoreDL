d55b1927d547a2497feb6705e20fb423686ce375
Nick <nick-ls@users.noreply.github.com>
2020-09-07 15:18:38 -0700

    Allow PNG files to substitute for SVG files
    Not every score has svg files available, so in that case, png files are used instead as a backup now

--- content.js
+++ content.js
@@ -3,7 +3,7 @@
 		for (let item of list) {
 			let img = item.target.querySelector("div > div > div > img");
 			if (img) {
-				if (img.src.match(/.*(?<=https?:\/\/musescore\.com\/static.*\/)(\d*)(?=\/[^\/]*\/score_\d+\.svg).*(?=score_0.svg)/)) {
+				if (img.src.match(/.*(?<=https?:\/\/musescore\.com\/static.*\/)(\d*)(?=\/[^\/]*\/score_0\.(?:svg|png)).*(?=score_0.(svg|png))/)) {
 					chrome.runtime.sendMessage({
 						url: img.src,
 						name: document.querySelector("h1").innerText
